9a808ba9665c90f8d8ff8564bae44a0add277d77
Piyush Gilada <131459191+Piyushgilada@users.noreply.github.com>
2025-09-09 12:49:27 +0530

    Add files via upload

---
+++ Train Data Engineering Project/SQL_DATABASE/dbo/Tables/Railway_info.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Railway_info] (
+
+	[Train_No] bigint NULL, 
+	[Train_Name] varchar(8000) NULL, 
+	[Source_Station_Name] varchar(8000) NULL, 
+	[Destination_Station_Name] varchar(8000) NULL, 
+	[days] varchar(8000) NULL, 
+	[day_type] varchar(8000) NULL
+);
